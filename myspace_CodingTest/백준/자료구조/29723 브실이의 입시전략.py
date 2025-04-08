706ece6070eab5d7af162fefd596d806ee57c142
dlwjdtn1112 <dlwjdtn0103@naver.com>
2025-04-08 10:25:12 +0900

    "백준 29723번 gpt 코드 추가"

--- myspace_CodingTest/백준/자료구조/29723 브실이의 입시전략.py
+++ myspace_CodingTest/백준/자료구조/29723 브실이의 입시전략.py
@@ -1,4 +1,4 @@
-
+# 내가 작성한 코드
 a,b,c = map(int,input().split())
 arr = []
 arr1 = []
@@ -31,3 +31,28 @@ arr_s.reverse()
 for i in range(b-c):
     max1  += arr_s[i][1]
 print(min1,max1)
+
+# chatGPT4o(개선된 코드)
+n, m, k = map(int, input().split())
+
+subject_scores = {}
+for _ in range(n):
+    name, score = input().split()
+    subject_scores[name] = int(score)
+
+known_subjects = [input() for _ in range(k)]
+
+# 공개된 과목 점수 합산
+base_score = sum(subject_scores[name] for name in known_subjects)
+
+# 공개되지 않은 과목 리스트
+remaining_scores = [score for name, score in subject_scores.items() if name not in known_subjects]
+
+# 최소 점수: 낮은 점수부터 m-k개 선택
+min_score = base_score + sum(sorted(remaining_scores)[:m-k])
+
+# 최대 점수: 높은 점수부터 m-k개 선택
+max_score = base_score + sum(sorted(remaining_scores, reverse=True)[:m-k])
+
+print(min_score, max_score)
+
