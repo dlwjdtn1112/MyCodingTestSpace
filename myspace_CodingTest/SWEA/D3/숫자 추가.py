693a49768104d62e871040d8a05c0af348f32100
dlwjdtn1112 <dlwjdtn0103@naver.com>
2025-07-27 22:25:17 +0900

    "SWEA 숫자 추가 D3"

---
+++ myspace_CodingTest/SWEA/D3/숫자 추가.py
@@ -0,0 +1,16 @@
+
+result = []
+
+n = int(input())
+
+for _ in range(n):
+    a,b,c = map(int,input().split())
+    l1 = list(map(int,input().split()))
+
+    for i in range(b):
+        x,y = map(int,input().split())
+        l1.insert(x,y)
+    result.append(l1[c])
+
+for  i in range(len(result)):
+    print("#"+str(i+1) , result[i])
