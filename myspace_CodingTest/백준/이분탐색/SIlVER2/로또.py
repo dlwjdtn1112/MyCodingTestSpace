5f196b1d9a7cb21cfd2fbe6dd0f6a55f58bedf34
dlwjdtn1112 <dlwjdtn0103@naver.com>
2026-02-03 16:41:56 +0900

    "백준 로또 6603번"

---
+++ myspace_CodingTest/백준/이분탐색/SIlVER2/로또.py
@@ -0,0 +1,81 @@
+from itertools import permutations
+
+while True:
+    result = []
+    l1 = list(map(int,input().split()))
+    if l1 == [0]:
+        break
+    else:
+        l2 = l1[1:len(l1)]
+        per = list(permutations(l2,6))
+        for lst in per:
+            if list(lst) == sorted(lst):
+                print(*lst)
+    print()
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
